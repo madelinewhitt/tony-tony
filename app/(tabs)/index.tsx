8fb3be55a31b3c0aa584dcf64a12283e98bca57f
madelinewhitt <madelinewhitton@MacBook-Air-10.local>
2024-08-28 13:59:29 -0400

    Added white noise volume and master volume controls

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,142 +1,162 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Button, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Button, Text, ScrollView, Pressable, TouchableOpacity } from "react-native";
 import Slider from "@react-native-community/slider";
-import { play, stop, getIsPlaying } from "expo-tone-synth";
+import { Inter_900Black, Inter_700Bold, Inter_100Thin, Inter_300Light, useFonts } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
+import FFTChart from "./FFT";
+import { Link } from "expo-router";
+import { stop, getIsPlaying, adjustFrequency, playWhiteNoise, adjustWhiteNoiseVolume, adjustMasterVolume } from "expo-tone-synth";
 
-const notes = [
-	{ name: "C", frequency: 261.63 },
-	{ name: "C#", frequency: 277.18 },
-	{ name: "D", frequency: 293.66 },
-	{ name: "D#", frequency: 311.13 },
-	{ name: "E", frequency: 329.63 },
-	{ name: "F", frequency: 349.23 },
-	{ name: "F#", frequency: 369.99 },
-	{ name: "G", frequency: 392.0 },
-	{ name: "G#", frequency: 415.3 },
-	{ name: "A", frequency: 440.0 },
-	{ name: "A#", frequency: 466.16 },
-	{ name: "B", frequency: 493.88 },
-];
+SplashScreen.preventAutoHideAsync();
 
-export default function HomeScreen() {
-	const [frequency, setFrequencyValue] = useState(440);
+export default function WhiteNoiseTest() {
 	const [isPlaying, setIsPlaying] = useState(false);
-	const [amplitudes, setAmplitudes] = useState([1.0, 0.5, 0.25]);
+	const [frequency, setFrequency] = useState(0.5);
+	const [whiteNoiseVolume, setWhiteNoiseVolume] = useState(0.5);
+	const [masterVolume, setMasterVolume] = useState(0.5);
+	const [fftData, setFftData] = useState([0, 0.1, 0.2, 0.3, 0.5, 0.3, 0.5, 0.2, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0, 0]);
 	const [adsr] = useState([0.1, 0.1, 0.7, 0.1]); // Default ADSR values
 
+	const [loaded, error] = useFonts({
+		Inter_700Bold,
+	});
+
 	useEffect(() => {
+		if (loaded || error) {
+			SplashScreen.hideAsync();
+		}
 		const checkIfPlaying = async () => {
 			const playing = await getIsPlaying();
 			setIsPlaying(playing);
 		};
 
 		checkIfPlaying();
-	}, []);
+	}, [loaded, error]);
 
-	const handlePlay = async () => {
-		console.log("Playing with frequency:", frequency);
-		console.log("Playing with amplitudes:", amplitudes);
-		await play(frequency, amplitudes, adsr);
+	if (!loaded && !error) {
+		return null;
+	}
+
+	const whiteNoise = async () => {
+		await playWhiteNoise();
 		setIsPlaying(true);
-	};
+	}
+
+	const handleFrequency = async () => {
+		await adjustFrequency(frequency);
+		console.log('Frequency adjusted to ', frequency);
+	}
+
+	const handleWhiteNoiseVolume = async () => {
+		await adjustWhiteNoiseVolume(whiteNoiseVolume);
+		console.log('White noise volume adjusted to ', whiteNoiseVolume);
+	}
+
+	const handleMasterVolume = async () => {
+		await adjustMasterVolume(masterVolume);
+		console.log('Master volume adjusted to ', masterVolume);
+	}
 
 	const handleStop = async () => {
 		await stop();
 		setIsPlaying(false);
-	};
-
-	const handleSetFrequency = async (newFrequency) => {
-		setFrequencyValue(newFrequency);
-		if (isPlaying) {
-			await play(newFrequency, amplitudes, adsr);
-		}
-	};
-
-	const handleSetAmplitude = async (index, value) => {
-		const newAmplitudes = [...amplitudes];
-		newAmplitudes[index] = value;
-		setAmplitudes(newAmplitudes);
-		console.log("Updated amplitudes:", newAmplitudes);
-		if (isPlaying) {
-			await play(frequency, newAmplitudes, adsr);
-		}
+		setFftData([]);
 	};
 
 	return (
-		<View style={styles.container}>
-			<View style={styles.noteButtonsContainer}>
-				{notes.map((note) => (
-					<TouchableOpacity
-						key={note.name}
-						style={styles.noteButton}
-						onPress={() => handleSetFrequency(note.frequency)}
-					>
-						<Text style={styles.noteButtonText}>{note.name}</Text>
-					</TouchableOpacity>
-				))}
+		<ScrollView>
+			<View style={styles.container}>
+				<Text style={styles.titleText}>Custom Noise Machine</Text>
+				<Text style={styles.miscText}>Frequency</Text>
+				<Slider
+					value={frequency}
+					onValueChange={(value) => {
+						setFrequency(value);
+						handleFrequency(); // Call the function to adjust frequency in real-time
+					}}
+					maximumValue={10000}
+					minimumValue={20}
+					step={1}
+					style={styles.slider}
+				/>
+				<Text style={styles.miscText}>Volume</Text>
+				<Slider
+					value={whiteNoiseVolume}
+					onValueChange={(value) => {
+						setWhiteNoiseVolume(value);
+						handleWhiteNoiseVolume();
+					}}
+					maximumValue={1}
+					minimumValue={0}
+					step={0.01}
+					style={styles.slider}
+				/>
+				<Text style={styles.miscText}>Master Volume</Text>
+				<Slider
+					value={masterVolume}
+					onValueChange={(value) => {
+						setMasterVolume(value);
+						handleMasterVolume(); // Call the function to adjust frequency in real-time
+					}}
+					maximumValue={1}
+					minimumValue={0}
+					step={0.01}
+					style={styles.slider}
+				/>
+				{/* make this a play pause button instead */}
+				<Button title="Play White Noise" onPress={whiteNoise} disabled={isPlaying} />
+				<Button title="Stop" onPress={handleStop} disabled={!isPlaying} />
+				<Link href="../(tabs)/SoundLibrary" asChild style={styles.HomeButton}>
+					<Pressable>
+						<Text style={styles.buttonText}>Background Sounds</Text>
+					</Pressable>
+				</Link>
+				<Link href="../(tabs)/SoundLibrary" asChild style={styles.HomeButton}>
+					<Pressable>
+						<Text style={styles.buttonText}>Favorites</Text>
+					</Pressable>
+				</Link>
+				<Link href="../(tabs)/SoundLibrary" asChild style={styles.HomeButton}>
+					<Pressable>
+						<Text style={styles.buttonText}>Save Preset</Text>
+					</Pressable>
+				</Link>
+				{/* <FFTChart data={fftData} /> */}
 			</View>
-			<Text>Frequency: {frequency} Hz</Text>
-			<Slider
-				style={styles.slider}
-				minimumValue={20}
-				maximumValue={9000}
-				value={frequency}
-				onValueChange={setFrequencyValue}
-				onSlidingComplete={handleSetFrequency}
-			/>
-			<Button title="Play Tone" onPress={handlePlay} disabled={isPlaying} />
-			<Button title="Stop Tone" onPress={handleStop} disabled={!isPlaying} />
-			<Text>Amplitudes</Text>
-			{amplitudes.map((amplitude, index) => (
-				<View key={index} style={styles.sliderContainer}>
-					<Text>
-						Amplitude {index + 1}: {amplitude.toFixed(2)}
-					</Text>
-					<Slider
-						style={styles.slider}
-						minimumValue={0}
-						maximumValue={1}
-						value={amplitude}
-						onValueChange={(value) => handleSetAmplitude(index, value)}
-					/>
-				</View>
-			))}
-		</View>
+		</ScrollView>
 	);
 }
 
 const styles = StyleSheet.create({
+	titleText: {
+		color: '#007AFF',
+		fontSize: 22,
+		fontFamily: "Inter_700Bold",
+		padding: 7,
+	},
+	miscText: {
+		color: '#007AFF',
+		fontSize: 18,
+	},
 	container: {
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center",
 		padding: 10,
 	},
-	noteButtonsContainer: {
-		flexDirection: "row",
-		flexWrap: "wrap",
-		justifyContent: "center",
-		marginBottom: 20,
-	},
-	noteButton: {
-		width: 40,
-		height: 40,
-		margin: 5,
-		justifyContent: "center",
-		alignItems: "center",
-		backgroundColor: "#1E90FF",
-		borderRadius: 5,
-	},
-	noteButtonText: {
-		color: "white",
-		fontSize: 16,
-	},
-	sliderContainer: {
-		width: "80%",
-		marginTop: 10,
-	},
 	slider: {
 		width: "100%",
 		height: 40,
+	},
+	HomeButton: {
+		backgroundColor: '#007AFF',
+		borderRadius: 50,
+		padding: 10,
+		color: '#fff',
+		fontSize: 18,
+	},
+	buttonText: {
+		color: '#fff',
+		fontSize: 18,
 	},
 });
